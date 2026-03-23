0fb8ca016a9c4e1a57e672c5bd8a8157b1b92ef5
TheFermiSea <squires.b@gmail.com>
2026-03-23 18:24:57 -0500

    auto: save agent edit to crates/common/src/core.rs

--- crates/common/src/core.rs
+++ crates/common/src/core.rs
@@ -268,6 +268,7 @@ impl From<&ImageMetadata> for BinaryImageMetadata {
             roi_origin: value.roi_origin,
             frame_number: value.frame_number,
             sequence_gap_from_previous: value.sequence_gap_from_previous,
+            summing_count: value.summing_count,
         }
     }
 }
