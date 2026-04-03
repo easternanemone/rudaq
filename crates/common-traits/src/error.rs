4896debbe9f1093a92c22689c00f5996e201faf1
Brian Squires <squires.b@gmail.com>
2026-04-03 23:45:07 +0000

    auto: save agent edit to crates/common-traits/src/error.rs

--- crates/common-traits/src/error.rs
+++ crates/common-traits/src/error.rs
@@ -276,7 +276,7 @@ pub enum DaqError {
     ///
     /// # Example
     ///
-    /// ```rust
+    /// ```rust,ignore
     /// use common::error::DaqError;
     ///
     /// const CAMERA_FAULT: u32 = 0x01;
